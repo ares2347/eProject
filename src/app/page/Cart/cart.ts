17451f4dbad13523d804a824c86ee6adce31fa2d
ares_2347 <trantu2000hp@gmail.com>
2022-08-23 21:04:17 +0700

    23/08

--- src/app/page/Cart/cart.ts
+++ src/app/page/Cart/cart.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import {Component, Output} from "@angular/core";
+import {cart} from "../../services/sharedData";
 
 @Component({
     selector: 'cart-page',
@@ -6,6 +7,38 @@ import { Component } from "@angular/core";
     styleUrls: ['./cart.css']
 })
 
+
 export class Cart {
+
+    @Output()
+    cart = [...cart]
+    coupon = 'IMRICH'
+    appliedCoupon = ''
+    subtotal= cart.reduce((pre:any, cur:any) => {
+        return(pre + cur.quantity*cur.price)
+    },0);
+    discount = 0;
+    total = this.subtotal - this.discount
+    recalculate = () => {
+        this.subtotal= cart.reduce((pre:any, cur:any) => {
+            return(pre + cur.quantity*cur.price)
+        },0);
+        this.total = this.subtotal - this.discount
+    }
+    onChange = (event:Event) => {
+        const idx = (event.target as HTMLInputElement).id as unknown as number
+        cart[idx].quantity = (event.target as HTMLInputElement).value as unknown as number;
+        this.recalculate()
+    }
+
+    onCouponChange= (event:Event) => {
+        this.appliedCoupon=(event.target as HTMLInputElement).value.toUpperCase()
+    }
+
+    onCouponApply = () => {
+        if (this.appliedCoupon == this.coupon) this.discount=200000
+        this.recalculate()
+    }
+
 
 }
